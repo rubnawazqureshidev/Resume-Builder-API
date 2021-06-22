5ec0bd9e6dc8376d8fbca8be5fe62bbb8ae9e90f
Rubnawaz Qureshi <rubnawazqureshi@yahoo.com>
2021-06-22 17:12:16 +0500

    implemented id & template3

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const { nanoid } = require('nanoid');
-const libre = require('libreoffice-convert');
 const fs = require('fs');
-const path = require('path');
 const { createReport } = require('docx-templates');
-const { convert } = require('html-to-text');
-const os = require('os');
 
 app.use('/download', express.static(__dirname + "/tmp"));
 app.use(cors());
 app.use(express.json());
 
-app.post('/', async (req, res) => {    
+app.post('/:id', async (req, res) => {   
+    const id = req.params.id;
+
     var { basics, work, education,  achievements, languages, skills, references, picture } = req.body;
     var { name, titreCv, profiles, email, phone, website, location } = basics;
     var { address } = location;
@@ -22,7 +20,7 @@ app.post('/', async (req, res) => {
         // generate doc
         var randomCharacters = nanoid(10);        
         var user_avatar = picture.replace(/^data:image\/\w+;base64,/, "");        
-        const template  = fs.readFileSync(__dirname +'/template2.docx');    
+        const template  = fs.readFileSync(__dirname +`/template${id}.docx`);    
 
         const buffer = await createReport({
             template,
@@ -47,24 +45,7 @@ app.post('/', async (req, res) => {
             processLineBreaks: true      
           });
           
-        fs.writeFileSync(__dirname +`/tmp/${randomCharacters}.docx`, buffer);
-
-        // converting doc to pdf libreoffice for ubuntu
-
-        // const extend = '.pdf'
-        // const enterPath = `./tmp/${randomCharacters}.docx`;
-        // const outputPath = `./tmp/${randomCharacters}${extend}`;
-        
-        // // Read file
-        // const file = fs.readFileSync(enterPath);
-        // // Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
-        // libre.convert(file, extend, undefined, (err, done) => {
-        //     if (err) {
-        //         console.log(`Error converting file: ${err}`);
-        //     }            
-        //     // Here in done you have pdf file which you can save or transfer in another stream
-        //     fs.writeFileSync(outputPath, done);
-        // });
+        fs.writeFileSync(__dirname +`/tmp/${randomCharacters}.docx`, buffer);      
     }
     catch(err) { 
         console.log(err);
@@ -74,7 +55,6 @@ app.post('/', async (req, res) => {
         success: true,
         download: {
             doc: `http://173.230.130.56:3000/download/${randomCharacters}.docx`
-            // pdf: `http://173.230.130.56:3000/download/${randomCharacters}.pdf`
         }
     });    
 });
